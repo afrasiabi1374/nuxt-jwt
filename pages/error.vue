288b19138f672bf61b87bf30df3e8b73944722bd
ali <mohammad.aligiga@gmail.com>
2022-08-30 18:03:08 +0430

    error handling

--- pages/error.vue
+++ pages/error.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
-    <h1>ERROR!</h1>
+    <div v-if="$fetchState.pending">
+      <h1>LOADING ...</h1>
+    </div>
+    <div v-else-if="list.length > 0">
+      <h1>ERROR!</h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'error',
-  asyncData({$api}){
-    // به این دلیل داخل فچ نمینویسیم چون باعث میشه بره توی خارج از لی اوت ارورمون
-    return $api._get('/posts')
+  data(){
+    return {
+      list:[]
+    }
+  },
+  // asyncData({$api}){
+  //   // به این دلیل داخل فچ نمینویسیم چون باعث میشه بره توی خارج از لی اوت ارورمون
+  //   return $api._get('/posts')
+  //   .then((res)=>{
+  //     console.log('res ',res);
+  //   })
+  // }
+  fetch(){
+    return this.$api._get('/posts')
     .then((res)=>{
-      console.log('res ',res);
+      console.log('res ',res)
+      if (res !== undefined) {      
+        console.log('res is OK!',res)  
+        this.list = res
+      }
     })
   }
 }
