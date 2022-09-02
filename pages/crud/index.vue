df8ef50b89a995ca9661ddce548244b83bedd823
afrasiabi1374 <mohammad.aligiga@gmail.com>
2022-09-02 21:36:49 +0430

    d

--- pages/crud/index.vue
+++ pages/crud/index.vue
@@ -3,15 +3,29 @@
         <!--  هست i18n مال localePath  -->
         <nuxt-link :to="localePath({ name: 'crud-create' })"> create </nuxt-link>
         <views-crud-filter-form @onSubmit="service"></views-crud-filter-form>
-        <div v-for="item in posts" :key="item.id" class="post-item">
+        <template v-if="isEmpty">
             <div>
-                <div class="post-item--title">{{ item.title }}</div>
-                <div class="post-item--body">{{ item.body }}</div>
+                لیست خالیه برادر
             </div>
-            <div>
-                <nuxt-link :to="localePath({ name: 'crud-update-id', params: { id: item.id } })">update</nuxt-link>
+        </template>
+        <template v-if="$fetchState.pending">
+            <div>loading...</div>
+        </template>
+        <template v-else>
+            <div v-for="item in posts" :key="item.id" :class="['post-item', loading ? 'post-item--loading' : '']">
+                <div>
+                    <div class="post-item--title">{{ item.title }}</div>
+                    <div class="post-item--body">{{ item.body }}</div>
+                </div>
+                <div>
+                    <nuxt-link :to="localePath({ name: 'crud-update-id', params: { id: item.id } })">update</nuxt-link>
+                </div>
+                <div @click="deleteItem(item.id)">
+                    delete
+                </div>
             </div>
-        </div>
+        </template>
+
     </div>
 </template>
 <script>
@@ -22,13 +36,31 @@ export default {
     },
     data(){
         return {
-            posts:[]
+            posts:[],
+            loading: false
         }
     },
     methods:{
         async service(params={}){
+            this.loading = true
             this.posts = await this.$axios
-            .$get('https://jsonplaceholder.typicode.com/posts', {params})   
+            .$get('https://jsonplaceholder.typicode.com/posts', {params})  
+            this.loading = false 
+        },
+        deleteItem(id){
+            this.$axios.$delete("https://jsonplaceholder.typicode.com/posts/"+id).then(()=>{
+            // از اسپلایس یا فیلتر هم میشه استفاده کرد
+            // اگه پجینیشن داریم فچ رو رفرش کنیم البته در حالت عادیم میشه
+            this.posts = this.posts.filter(item => item.id !== id)
+            })
+            .catch((e)=>{
+                console.log(e)
+            })
+        }
+    },
+    computed: {
+        isEmpty(){
+            return !this.loading && !this.$fetchState.pending && this.posts.length === 0
         }
     }
 }
@@ -49,4 +81,8 @@ export default {
             color: dimgray;
         }
     }
+
+.post-item--loading{
+    opacity: .2;
+}
 </style>
